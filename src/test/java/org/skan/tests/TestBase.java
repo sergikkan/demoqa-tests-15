7aee22be2122250ddf7779c29eb1db3ec2a2fa60
serhii kan <skan@one.kz>
2022-10-05 15:09:30 +0600

    final version with faker

---
+++ src/test/java/org/skan/tests/TestBase.java
@@ -0,0 +1,37 @@
+package org.skan.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.skan.pages.RegistrationFormPage;
+
+import org.skan.utils.Month;
+
+public class TestBase {
+
+    Faker faker= new Faker();
+
+    String firstName=faker.name().firstName(),
+            lastName=faker.name().lastName(),
+            email=faker.internet().emailAddress(),
+            gender=faker.demographic().sex(),
+            phone=faker.phoneNumber().subscriberNumber(10),
+            day= String.valueOf(faker.number().numberBetween(1,28)),
+            month= String.valueOf(Month.getRandomMonth()),
+            year= String.valueOf(faker.number().numberBetween(1970,2004)),
+            address=faker.address().fullAddress(),
+            subject="Maths",
+            hobbies="Sports",
+            image="testpicture.jpeg",
+            state="NCR",
+            city="Delhi";
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    @BeforeAll
+    static void setUp(){
+        Configuration.baseUrl="https://demoqa.com";
+        Configuration.holdBrowserOpen=true;
+        //Configuration.pageLoadTimeout=150000;
+        Configuration.browserSize="1920x1080";
+    }
+}
